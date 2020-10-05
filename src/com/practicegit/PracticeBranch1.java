efc9d5f7dec1f8ddfef38dce6611789237bf4d2c
Sanjana Jain <sanjana.jain04@gmail.com>
2020-10-05 15:06:44 +0530

    changes to branch 1

---
+++ src/com/practicegit/PracticeBranch1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.practicegit;
+
+/**
+ * @author hp
+ *
+ */
+public class PracticeBranch1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcomr to branch 1");
+
+	}
+
+}
